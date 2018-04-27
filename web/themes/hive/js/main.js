e30f4c0816f1bfcc5d2b496af808a804578adb25
Jeremy Rasmussen <jeremy@jprasmussen.com>
2018-04-27 06:28:16 -0600

    Session Schedule: - Added Body to view - more/less toggle - presenters field

--- web/themes/hive/js/main.js
+++ web/themes/hive/js/main.js
@@ -1,2 +1,10 @@
 (function ($) {
+  Drupal.behaviors.moreSession = {
+    attach: function (context, settings) {
+      $( ".session-toggle" ).click(function() {
+        $(this).parent( ".session-description" ).toggleClass('closed').toggleClass('opened');
+      });
+    }
+  };
+
 })(jQuery);
